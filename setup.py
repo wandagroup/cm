d5584eb1e3ceb53da26a2c71bb74e5b7352a5d20
wandagroup <kongdezhi@wondersgroup.com>
2019-02-26 15:57:01 +0800

    change botocore version in requirements.txt and setup.py

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     install_requires=[
         'bleach>=3.0.2, <4.0.0',
         'boto3>=1.4.7, <2.0.0',
-        'botocore>=1.7.0, <1.8.0',
+        'botocore>=1.12.102, <1.13.0',
         'celery>=4.2.0, <5.0.0',
         'click>=6.0, <7.0.0',  # click >=7 forces "-" instead of "_"
         'colorama',
