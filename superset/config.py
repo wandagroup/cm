2a397071b67327b550333ff0b12c04bf38df6f90
wandagroup <kongdezhi@wondersgroup.com>
2019-02-26 16:21:09 +0800

    change 182 mysql username & password

--- superset/config.py
+++ superset/config.py
@@ -55,7 +55,7 @@ SECRET_KEY = '\2\1thisismyscretkey\1\2\e\y\y\h'  # noqa
 # The SQLAlchemy connection string.
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(DATA_DIR, 'superset.db')
 # SQLALCHEMY_DATABASE_URI = 'mysql://myapp@localhost/myapp'
-SQLALCHEMY_DATABASE_URI = 'mysql://root:wonders@localhost/cmdata?charset=utf8'
+SQLALCHEMY_DATABASE_URI = 'mysql://wanda:wonders@192.168.12.182/cmdata?charset=utf8'
 # SQLALCHEMY_DATABASE_URI = 'postgresql://root:password@localhost/myapp'
 
 # In order to hook up a custom password store for all SQLACHEMY connections
